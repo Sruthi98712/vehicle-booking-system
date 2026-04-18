aeefc7f044558cdf9e43b34cee022366041fc621
SRUTHI <sruthikuriti123@gmail.com>
2026-04-18 21:27:10 +0530

    Update user.model.js

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -10,7 +10,7 @@ const userSchema = new mongoose.Schema({
     enum: ['admin', 'vendor', 'customer'],
     default: 'customer'
   },
-  phone: { type: String, required: false }, // Removed default value to avoid duplicate empty strings
+  phone: { type: String, default: '' },
   mobile: { type: String, unique: true, required: true },
   isMobileVerified: { type: Boolean, default: false },
   googleId: { type: String, unique: true, sparse: true },
